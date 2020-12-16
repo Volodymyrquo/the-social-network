9df66697ed72d55a60e9fdce86d37855b2ef1f71
Volodymyr <volodymyrquo@gmail.com>
2020-12-16 07:01:23 +0200

    added typescript to auth-reducer

---
+++ src/redux/auth-reducer.ts
@@ -0,0 +1,86 @@
+import { authAPI } from "../api/api";
+import { stopSubmit } from "redux-form";
+
+const SET_USER_DATA = "SET_USER_DATA";
+
+
+export type InitialStateType = {
+  email: number | null
+  id: string | null
+  login: string | null
+  isAuth: boolean
+  captchaUrl: string | null
+};
+const initialState:InitialStateType = {
+  email: null,
+  id: null,
+  login: null,
+  isAuth: false,
+  captchaUrl: null
+
+};
+
+const authReducer = (state = initialState, action: any):InitialStateType => {
+  switch (action.type) {
+    case SET_USER_DATA:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+type SetAuthUserDataActionPayloadType = {
+   email:string|null;
+   id:number|null;
+   login:string|null;
+   isAuth:boolean;
+}
+type SetAuthUserDataActionType = {
+  type: typeof SET_USER_DATA;
+  payload: SetAuthUserDataActionPayloadType;
+
+}
+
+
+export const setAuthUserData = (email:string|null, id:number|null, login:string|null, isAuth:boolean):SetAuthUserDataActionType => ({
+  type: SET_USER_DATA,
+  payload: { email, id, login, isAuth },
+});
+
+
+export const getAuthUserData = () => async (dispatch:any) => {
+  const data = await authAPI.me();
+  if (data.resultCode === 0) {
+    let { email, id, login } = data.data;
+    dispatch(setAuthUserData(email, id, login, true));
+  }
+};
+
+type GetLoginUserDataType = {
+  email:string|null;
+  password:string|null;
+  rememberMe:boolean
+}
+
+export const getLoginUserData = ({ email, password, rememberMe}:GetLoginUserDataType) => async (
+  dispatch:any
+) => {
+  const data = await authAPI.login({ email, password, rememberMe });
+  if (!data.resultCode) {
+    dispatch(getAuthUserData());
+  } else {
+    dispatch(stopSubmit("login", { _error: data.messages }));
+  }
+};
+
+export const logoutUser = () => async (dispatch:any) => {
+  const data = await authAPI.logout();
+  if (!data.resultCode) {
+    dispatch(setAuthUserData(null, null, null, false));
+  }
+};
+
+export default authReducer;
